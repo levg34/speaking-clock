97d8e5334329360bda05a98c04d06b056ed48ba5
levg34 <levg34@users.noreply.github.com>
2017-11-16 11:42:26 +0100

    test with textbox and button

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var app = angular.module('app', [])
 
 app.controller('aCtrl', function($scope,$http) {
+	$scope.text = 'écrivez dans la case'
 	$scope.loadConfig = function () {
 		meSpeak.loadConfig('mespeak/mespeak_config.json')
 		meSpeak.loadVoice('mespeak/voices/fr.json')
@@ -8,5 +9,9 @@ app.controller('aCtrl', function($scope,$http) {
 	$scope.speak = function (text) {
 		meSpeak.speak(text, {variant: 'whisperf'})
 	}
+	$scope.speakSentence = function () {
+		$scope.speak($scope.text)
+	}
 	$scope.loadConfig()
+	$scope.speakSentence()
 })
